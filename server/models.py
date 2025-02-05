46484f94662368f572388ec2f43ccc0291980c8e
Ingeborg Adolfs <ingeborgadolfs@gmail.com>
2025-02-04 18:30:15 -0700

    modified defaults

--- server/models.py
+++ server/models.py
@@ -14,6 +14,6 @@ class Message(db.Model, SerializerMixin):
     id = db.Column(db.Integer, primary_key=True)
     body = db.Column(db.String)
     username = db.Column(db.String)
-    created_at = db.Column(db.DateTime, server_default=db.func.now())
-    updated_at = db.Column(db.DateTime, onupdate=db.func.now())
+    created_at = db.Column(db.DateTime, default=db.func.now())
+    updated_at = db.Column(db.DateTime, default =db.func.now(), onupdate=db.func.now())
 
